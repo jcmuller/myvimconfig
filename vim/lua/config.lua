c4f5b5396cf4e40b906f7c6a64b902496caec47d
Juan C. Müller <jcmuller@gmail.com>
2022-02-16 19:44:36 -0500

    Remove commented out

--- vim/lua/config.lua
+++ vim/lua/config.lua
@@ -1,10 +1,7 @@
 -- lsp {{{
 local nvim_lsp = require('lspconfig')
--- local configs = require("lspconfig/configs")
--- local util = require("lspconfig/util")
 local lsp_signature = require("lsp_signature")
 
--- local capabilities = vim.lsp.protocol.make_client_capabilities()
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
