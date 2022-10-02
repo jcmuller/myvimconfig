b093dbd1d20ad61c9d64583a99aab86f45b0ce98
Juan C. Müller <jcmuller@gmail.com>
2022-10-01 20:53:52 -0400

    Configure tagfunc and formatexpr

--- vim/lua/config.lua
+++ vim/lua/config.lua
@@ -18,8 +18,10 @@ local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
+  buf_set_option('tagfunc', 'v:lua.vim.lsp.tagfunc')
   --Enable completion triggered by <c-x><c-o>
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  buf_set_option('formatexpr', 'v:lua.vim.lsp.formatexpr')
 
   -- Mappings.
   local opts = { noremap=true, silent=true }
