d59da588a246e6f5fd6413a305cc797fbe55782c
Juan C. Müller <jcmuller@gmail.com>
2022-10-01 20:58:00 -0400

    Add ccls

--- vim/lua/config.lua
+++ vim/lua/config.lua
@@ -78,6 +78,7 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local servers = {
+  "ccls",
   "regols",
   "sorbet",
   "sourcekit",
