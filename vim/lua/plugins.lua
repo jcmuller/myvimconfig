5f27de7c257831f2b05014e8ee8117cd8f520ad0
Juan C. Müller <jcmuller@gmail.com>
2022-12-19 22:07:30 -0500

    Fix mappings

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -508,27 +508,18 @@ return require('packer').startup(function(use)
           }
         },
         mapping = {
-          ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-          ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-          ['<C-Space>'] = cmp.mapping(cmp.mapping.complete({}), { 'i', 'c' }),
-          ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping({
             i = cmp.mapping.abort(),
             c = cmp.mapping.close(),
           }),
           ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<Tab>"] = cmp.mapping(
-            function(fallback)
-              cmp_ultisnips_mappings.expand_or_jump_forwards(fallback)
-            end,
-            { "i", "s", "c" }
-          ),
-          ["<S-Tab>"] = cmp.mapping(
-            function(fallback)
-              cmp_ultisnips_mappings.jump_backwards(fallback)
-            end,
-            { "i", "s", "c" }
-          ),
+          ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item()),
+          ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item()),
+          ['<Down>'] = cmp.mapping(cmp.mapping.select_next_item()),
+          ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item()),
 
           -- Work with UltiSnips
           -- ["<Tab>"] = cmp.mapping({
